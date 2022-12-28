41d9171a961a66ce3e2f26270031d9c8ad61ea87
Dimethoxy <dimethoxy.audio@gmail.com>
2022-12-28 02:30:36 +0100

    PresetManager

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,38 +1,44 @@
 /*
   ==============================================================================
 
-	This file contains the basic framework code for a JUCE plugin editor.
+        This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
 
 //==============================================================================
-NeutrinoAudioProcessorEditor::NeutrinoAudioProcessorEditor(NeutrinoAudioProcessor& p)
-	: AudioProcessorEditor(&p), audioProcessor(p),
-	genericAudioProcessorEditor(p)
+NeutrinoAudioProcessorEditor::NeutrinoAudioProcessorEditor(
+  NeutrinoAudioProcessor& p)
+  : AudioProcessorEditor(&p)
+  ,
+
+  audioProcessor(p)
+  , presetPanel(p.getPresetManager())
+  , genericAudioProcessorEditor(p)
 {
-	addAndMakeVisible(presetPanel);
-	addAndMakeVisible(genericAudioProcessorEditor);
-	setSize(400, 400);
+  addAndMakeVisible(presetPanel);
+  addAndMakeVisible(genericAudioProcessorEditor);
+  setSize(400, 400);
 }
 
-NeutrinoAudioProcessorEditor::~NeutrinoAudioProcessorEditor()
-{
-}
+NeutrinoAudioProcessorEditor::~NeutrinoAudioProcessorEditor() {}
 
 //==============================================================================
-void NeutrinoAudioProcessorEditor::paint(juce::Graphics& g)
+void
+NeutrinoAudioProcessorEditor::paint(juce::Graphics& g)
 {
-	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+  g.fillAll(
+    getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 }
 
-void NeutrinoAudioProcessorEditor::resized()
+void
+NeutrinoAudioProcessorEditor::resized()
 {
-	auto bounds = getLocalBounds();
+  auto bounds = getLocalBounds();
 
-	genericAudioProcessorEditor.setBoundsRelative(0.0f, 0.2f, 1.0f, 0.8f);
-	presetPanel.setBounds(bounds.removeFromTop(proportionOfHeight(0.1)));
+  genericAudioProcessorEditor.setBoundsRelative(0.0f, 0.2f, 1.0f, 0.8f);
+  presetPanel.setBounds(bounds.removeFromTop(proportionOfHeight(0.1)));
 }
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-	This file contains the basic framework code for a JUCE plugin processor.
+        This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
@@ -11,176 +11,206 @@
 
 //==============================================================================
 NeutrinoAudioProcessor::NeutrinoAudioProcessor()
-	: AudioProcessor(BusesProperties().withOutput("Output", juce::AudioChannelSet::stereo(), true)),
-	valueTreeState(*this, nullptr, ProjectInfo::projectName, dmt::createParameterLayout())
+  : AudioProcessor(BusesProperties().withOutput("Output",
+                                                juce::AudioChannelSet::stereo(),
+                                                true))
+  , valueTreeState(*this,
+                   nullptr,
+                   ProjectInfo::projectName,
+                   dmt::createParameterLayout())
 {
+  valueTreeState.state.setProperty(
+    dmt::PresetManager::presetNameProperty, "", nullptr);
+  valueTreeState.state.setProperty(
+    "version", ProjectInfo::versionString, nullptr);
+  presetManager = std::make_unique<dmt::PresetManager>(valueTreeState);
 }
 
-NeutrinoAudioProcessor::~NeutrinoAudioProcessor()
-{
-}
+NeutrinoAudioProcessor::~NeutrinoAudioProcessor() {}
 
 //==============================================================================
-const juce::String NeutrinoAudioProcessor::getName() const
+const juce::String
+NeutrinoAudioProcessor::getName() const
 {
-	return JucePlugin_Name;
+  return JucePlugin_Name;
 }
 
-bool NeutrinoAudioProcessor::acceptsMidi() const
+bool
+NeutrinoAudioProcessor::acceptsMidi() const
 {
 #if JucePlugin_WantsMidiInput
-	return true;
+  return true;
 #else
-	return false;
+  return false;
 #endif
 }
 
-bool NeutrinoAudioProcessor::producesMidi() const
+bool
+NeutrinoAudioProcessor::producesMidi() const
 {
 #if JucePlugin_ProducesMidiOutput
-	return true;
+  return true;
 #else
-	return false;
+  return false;
 #endif
 }
 
-bool NeutrinoAudioProcessor::isMidiEffect() const
+bool
+NeutrinoAudioProcessor::isMidiEffect() const
 {
 #if JucePlugin_IsMidiEffect
-	return true;
+  return true;
 #else
-	return false;
+  return false;
 #endif
 }
 
-double NeutrinoAudioProcessor::getTailLengthSeconds() const
+double
+NeutrinoAudioProcessor::getTailLengthSeconds() const
 {
-	return 0.0;
+  return 0.0;
 }
 
-int NeutrinoAudioProcessor::getNumPrograms()
+int
+NeutrinoAudioProcessor::getNumPrograms()
 {
-	return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-	// so this should be at least 1, even if you're not really implementing programs.
+  return 1; // NB: some hosts don't cope very well if you tell them there are 0
+            // programs,
+  // so this should be at least 1, even if you're not really implementing
+  // programs.
 }
 
-int NeutrinoAudioProcessor::getCurrentProgram()
+int
+NeutrinoAudioProcessor::getCurrentProgram()
 {
-	return 0;
+  return 0;
 }
 
-void NeutrinoAudioProcessor::setCurrentProgram(int index)
+void
+NeutrinoAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String NeutrinoAudioProcessor::getProgramName(int index)
+const juce::String
+NeutrinoAudioProcessor::getProgramName(int index)
 {
-	return {};
+  return {};
 }
 
-void NeutrinoAudioProcessor::changeProgramName(int index, const juce::String& newName)
+void
+NeutrinoAudioProcessor::changeProgramName(int index,
+                                          const juce::String& newName)
 {
 }
 
 //==============================================================================
-void NeutrinoAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
+void
+NeutrinoAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-	// Use this method as the place to do any pre-playback
-	// initialisation that you need..
+  // Use this method as the place to do any pre-playback
+  // initialisation that you need..
 }
 
-void NeutrinoAudioProcessor::releaseResources()
+void
+NeutrinoAudioProcessor::releaseResources()
 {
-	// When playback stops, you can use this as an opportunity to free up any
-	// spare memory, etc.
+  // When playback stops, you can use this as an opportunity to free up any
+  // spare memory, etc.
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool NeutrinoAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+bool
+NeutrinoAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
 #if JucePlugin_IsMidiEffect
-	juce::ignoreUnused(layouts);
-	return true;
+  juce::ignoreUnused(layouts);
+  return true;
 #else
-	// This is the place where you check if the layout is supported.
-	// In this template code we only support mono or stereo.
-	// Some plugin hosts, such as certain GarageBand versions, will only
-	// load plugins that support stereo bus layouts.
-	if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-		&& layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-		return false;
+  // This is the place where you check if the layout is supported.
+  // In this template code we only support mono or stereo.
+  // Some plugin hosts, such as certain GarageBand versions, will only
+  // load plugins that support stereo bus layouts.
+  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    return false;
 
-	// This checks if the input layout matches the output layout
-#if ! JucePlugin_IsSynth
-	if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-		return false;
+    // This checks if the input layout matches the output layout
+#if !JucePlugin_IsSynth
+  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+    return false;
 #endif
 
-	return true;
+  return true;
 #endif
 }
 #endif
 
-void NeutrinoAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void
+NeutrinoAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
+                                     juce::MidiBuffer& midiMessages)
 {
-	juce::ScopedNoDenormals noDenormals;
-	auto totalNumInputChannels = getTotalNumInputChannels();
-	auto totalNumOutputChannels = getTotalNumOutputChannels();
+  juce::ScopedNoDenormals noDenormals;
+  auto totalNumInputChannels = getTotalNumInputChannels();
+  auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-	// In case we have more outputs than inputs, this code clears any output
-	// channels that didn't contain input data, (because these aren't
-	// guaranteed to be empty - they may contain garbage).
-	// This is here to avoid people getting screaming feedback
-	// when they first compile a plugin, but obviously you don't need to keep
-	// this code if your algorithm always overwrites all the output channels.
-	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-		buffer.clear(i, 0, buffer.getNumSamples());
+  // In case we have more outputs than inputs, this code clears any output
+  // channels that didn't contain input data, (because these aren't
+  // guaranteed to be empty - they may contain garbage).
+  // This is here to avoid people getting screaming feedback
+  // when they first compile a plugin, but obviously you don't need to keep
+  // this code if your algorithm always overwrites all the output channels.
+  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+    buffer.clear(i, 0, buffer.getNumSamples());
 
-	// This is the place where you'd normally do the guts of your plugin's
-	// audio processing...
-	// Make sure to reset the state if your inner loop is processing
-	// the samples and the outer loop is handling the channels.
-	// Alternatively, you can process the samples with the channels
-	// interleaved by keeping the same state.
-	for (int channel = 0; channel < totalNumInputChannels; ++channel)
-	{
-		auto* channelData = buffer.getWritePointer(channel);
+  // This is the place where you'd normally do the guts of your plugin's
+  // audio processing...
+  // Make sure to reset the state if your inner loop is processing
+  // the samples and the outer loop is handling the channels.
+  // Alternatively, you can process the samples with the channels
+  // interleaved by keeping the same state.
+  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
+    auto* channelData = buffer.getWritePointer(channel);
 
-		// ..do something to the data...
-	}
+    // ..do something to the data...
+  }
 }
 
 //==============================================================================
-bool NeutrinoAudioProcessor::hasEditor() const
+bool
+NeutrinoAudioProcessor::hasEditor() const
 {
-	return true; // (change this to false if you choose to not supply an editor)
+  return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* NeutrinoAudioProcessor::createEditor()
+juce::AudioProcessorEditor*
+NeutrinoAudioProcessor::createEditor()
 {
-	return new NeutrinoAudioProcessorEditor(*this);
+  return new NeutrinoAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void NeutrinoAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
+void
+NeutrinoAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
-	const auto state = valueTreeState.copyState();
-	const auto xml = state.createXml();
-	copyXmlToBinary(*xml, destData);
+  const auto state = valueTreeState.copyState();
+  const auto xml = state.createXml();
+  copyXmlToBinary(*xml, destData);
 }
 
-void NeutrinoAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
+void
+NeutrinoAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
-	const auto xmlState = getXmlFromBinary(data, sizeInBytes);
-	if (xmlState == nullptr)
-		return;
-	const auto newTree = juce::ValueTree::fromXml(*xmlState);
-	valueTreeState.replaceState(newTree);
+  const auto xmlState = getXmlFromBinary(data, sizeInBytes);
+  if (xmlState == nullptr)
+    return;
+  const auto newTree = juce::ValueTree::fromXml(*xmlState);
+  valueTreeState.replaceState(newTree);
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessor* JUCE_CALLTYPE
+createPluginFilter()
 {
-	return new NeutrinoAudioProcessor();
+  return new NeutrinoAudioProcessor();
 }
