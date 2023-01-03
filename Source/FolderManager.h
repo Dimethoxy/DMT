23fa9fb1c964e8dcca70e069f6576682fd156d9a
Dimethoxy <dimethoxy.audio@gmail.com>
2023-01-03 04:49:40 +0100

    Added SynthVoice and SynthSound

--- Source/FolderManager.h
+++ Source/FolderManager.h
@@ -15,7 +15,7 @@
 //==============================================================================
 
 namespace dmt {
-class FolderManager
+class FolderManager : juce::ValueTree::Listener
 {
 public:
   inline static const juce::File& defaultDirectory =
@@ -23,5 +23,29 @@ public:
       juce::File::SpecialLocationType::commonDocumentsDirectory)
       .getChildFile(ProjectInfo::companyName)
       .getChildFile(ProjectInfo::projectName);
+
+  inline static const juce::String& folderNameProperty = "folderName";
+
+  FolderManager(juce::AudioProcessorValueTreeState& apvts)
+    : valueTreeState(apvts)
+  {
+    // Create default directory
+    if (!defaultDirectory.exists()) {
+      const auto result = defaultDirectory.createDirectory();
+      if (result.failed()) {
+        DBG("Could not create preset directory: " + result.getErrorMessage());
+        jassertfalse;
+      }
+    }
+    valueTreeState.state.addListener(this);
+    currentFolder.referTo(
+      valueTreeState.state.getPropertyAsValue(folderNameProperty, nullptr));
+  }
+
+private:
+  //==============================================================================
+  juce::AudioProcessorValueTreeState& valueTreeState;
+  juce::StringArray folderList;
+  juce::Value currentFolder;
 };
 }
