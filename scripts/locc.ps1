6520b31c9c82927afac6133181e5bef7044bbd4a
Lunix-420 <76759448+Lunix-420@users.noreply.github.com>
2023-11-27 02:22:00 +0100

    Refactor LOC counting script and add language-specific line count

--- scripts/locc.ps1
+++ scripts/locc.ps1
@@ -1,11 +1,42 @@
-$directoryPath = "./src"
+$directoryPath = "./"
 $totalLoc = 0
-$files = Get-ChildItem -Path $directoryPath -File -Include *.h,*.cpp -Recurse
+$locPerLanguage = @{}  # Hashtable to store lines per language
+
+# List of ignored folders
+$ignoredFolders = @("external", "build")
+
+# Display ignored folders
+Write-Host "------------------------------------------------------------"
+Write-Host "Ignored folders: $($ignoredFolders -join ', ')"
+Write-Host "------------------------------------------------------------"
+
+$files = Get-ChildItem -Path $directoryPath -File -Include *.h,*.cpp,CMakeLists.txt,*.ps1,*.sh -Recurse
 
 foreach ($file in $files) {
+    # Check if the file path contains any of the ignored subfolders and skip it if true
+    if ($ignoredFolders | Where-Object { $file.FullName -like "*\$_\*" }) {
+        continue
+    }
+
+    $extension = $file.Extension.TrimStart('.')
     $loc = (Get-Content $file.FullName | Measure-Object -Line).Lines
     Write-Host "File: $($file.FullName), LOC: $loc"
+
+    # Update lines count for the specific language
+    if ($locPerLanguage.ContainsKey($extension)) {
+        $locPerLanguage[$extension] += $loc
+    } else {
+        $locPerLanguage[$extension] = $loc
+    }
+
     $totalLoc += $loc
 }
 
-Write-Host "Total LOC in folder: $totalLoc"
+Write-Host "------------------------------------------------------------"
+
+# Display lines per language
+foreach ($language in $locPerLanguage.Keys) {
+    Write-Host "Total LOC for $language : $($locPerLanguage[$language])"
+}
+Write-Host "------------------------------------------------------------"
+Write-Host "Combined LOC in folder: $totalLoc"
