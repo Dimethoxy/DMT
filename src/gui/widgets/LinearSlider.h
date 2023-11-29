cf1af29e080f3af4b27aaab69969f03e97cb23c1
David <davidhess336@gmx.de>
2023-11-29 13:28:08 +0100

    Implemented LinearSlider.setBoundsByPoints()

--- src/gui/widgets/LinearSlider.h
+++ src/gui/widgets/LinearSlider.h
@@ -10,8 +10,11 @@ namespace widgets {
 class LinearSlider : public juce::Slider
 {
   using Settings = dmt::AppSettings;
+  using Slider = Settings::Slider;
   const float& size = Settings::Layout::size;
   const float& rawPadding = Settings::Slider::padding;
+  // Thumb
+  const float& thumbSize = Slider::thumbSize;
 
 public:
   enum Type
@@ -39,12 +42,48 @@ public:
   void paint(juce::Graphics& g) override
   {
     auto bounds = getLocalBounds();
-    int padding = (int)(rawPadding * size);
-    auto reducedBounds = bounds.reduced(padding);
 
     g.setColour(juce::Colours::red);
     g.fillRect(bounds);
+
+    switch (orientation) {
+      case Orientation::Horizontal:
+        drawHorizontal();
+        break;
+      case Orientation::Vertical:
+        drawVertical();
+        break;
+      default:
+        jassert(false);
+        break;
+    }
   }
+  void setBoundsByPoints(juce::Point<int> primaryPoint,
+                         juce::Point<int> secondaryPoint)
+  {
+    const int padding = (int)(rawPadding * size);
+    const float primaryX = (float)primaryPoint.getX();
+    const float primaryY = (float)primaryPoint.getY();
+    const float secondaryX = (float)primaryPoint.getX();
+    const float secondaryY = (float)primaryPoint.getY();
+
+    const float minSize = thumbSize * size;
+    const float xDistance = secondaryX - primaryX;
+    const float innerWidth = (minSize > xDistance) ? minSize : xDistance;
+    const float yDistance = secondaryY - primaryY;
+    const float innerHeight = (minSize > yDistance) ? minSize : yDistance;
+
+    const auto innerBounds =
+      juce::Rectangle<float>(primaryX, primaryY, innerWidth, innerHeight);
+    const auto centre = innerBounds.getCentre();
+
+    const auto outerBounds = innerBounds.expanded(padding).withCentre(centre);
+    this->setBounds(outerBounds.toNearestInt());
+  }
+
+protected:
+  void drawHorizontal() {}
+  void drawVertical() {}
 
 private:
   Type type;
