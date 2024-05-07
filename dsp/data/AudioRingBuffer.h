342af537f483c428fc3e4721e8ee83eb24fde0a7
Lunix-420 <76759448+Lunix-420@users.noreply.github.com>
2024-05-07 03:10:20 +0200

    Removed old Oscilloscope Classes

--- dsp/data/AudioRingBuffer.h
+++ dsp/data/AudioRingBuffer.h
@@ -5,12 +5,18 @@ namespace dsp {
 namespace data {
 //==============================================================================
 template<typename SampleType>
-class FifoBuffer : juce::AbstractFifo
+class AudioRingBuffer : juce::AbstractFifo
 {
   using BufferData = std::vector<std::vector<float>>;
   using ChannelData = std::vector<float>;
-
+  //============================================================================
 public:
+  AudioRingBuffer(int channels, int bufferSize)
+    : AbstractFifo(buffersize)
+  {
+    buffer.setSize(channels, buffersize);
+  }
+  //============================================================================
   void addToFifo(const juce::AudioBuffer<SampleType>& target)
   {
     // Prepare to write
@@ -21,42 +27,56 @@ public:
     // Block 1
     if (size1 > 0)
       for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
-        buffer.copyFrom(
-          channel, start1, samples.getReadPointer(channel), size1);
+        buffer.copyFrom(channel, start1, target.getReadPointer(channel), size1);
 
     // Block 2
     if (size2 > 0)
       for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
         buffer.copyFrom(
-          channel, start2, samples.getReadPointer(channel, size1), size2);
+          channel, start2, target.getReadPointer(channel, size1), size2);
 
     // Finish writing
     finishedWrite(size1 + size2);
   }
-
-  void readFromFifo(juce::AudioBuffer<FloatType>& samples, int numSamples = -1)
+  //============================================================================
+  void readFromFifo(juce::AudioBuffer<SampleType>& target, int numSamples = -1)
   {
     // Prepare to read
-    const int numSamples = samples.getNumSamples();
+    const int numSamples = target.getNumSamples();
     int start1, size1, start2, size2;
-    prepareToRead(readSamples, start1, size1, start2, size2);
+    prepareToRead(numSamples, start1, size1, start2, size2);
 
     // Block 1
     if (size1 > 0)
       for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
-        samples.copyFrom(
+        target.copyFrom(
           channel, 0, buffer.getReadPointer(channel, start1), size1);
 
     // Block 2
     if (size2 > 0)
       for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
-        samples.copyFrom(
+        target.copyFrom(
           channel, size1, buffer.getReadPointer(channel, start2), size2);
 
     // Finish reading
     finishedRead(size1 + size2);
   }
-
+  //============================================================================
+  void setSize(const int channels, const int newBufferSize)
+  {
+    buffer.setSize(channels, newBufferSize);
+    setTotalSize(newBufferSize);
+    reset();
+  }
+  //============================================================================
+  void clear()
+  {
+    buffer.clear();
+    reset();
+  }
+  //============================================================================
+  int getNumChannels() const { return buffer.getNumChannels(); }
+  //============================================================================
 private:
   juce::AudioBuffer<SampleType> buffer;
 };
