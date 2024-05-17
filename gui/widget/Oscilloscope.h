0222cfc3a0072adbe556d65562817bab7baae17e
Lunix-420 <76759448+Lunix-420@users.noreply.github.com>
2024-05-17 21:25:12 +0200

    Artefacts extremly improved

--- gui/widget/Oscilloscope.h
+++ gui/widget/Oscilloscope.h
@@ -12,13 +12,14 @@ class Oscilloscope : public juce::Component
   using RingBuffer = dmt::dsp::data::RingAudioBuffer<SampleType>;
   using PixelFormat = juce::Image::PixelFormat;
   using Image = juce::Image;
+  using Point = juce::Point<float>;
 
 public:
   //==============================================================================
   Oscilloscope(RingBuffer& ringBuffer, int channel)
     : ringBuffer(ringBuffer)
-    , lastFullyDrawnSampleValue(0)
-    , lastFullyDrawnSampleOffsetX(0)
+    , currentSample(0.0f)
+    , currentX(0.0f)
     , image(Image(PixelFormat::ARGB, 1, 1, true))
     , channel(channel)
   {
@@ -27,14 +28,15 @@ public:
   void resized() override
   {
     Image oldImage(image);
-    image = Image(Image::ARGB, jmax(1, getWidth()), jmax(1, getHeight()), true);
+    image =
+      Image(Image::ARGB, jmax(1, getWidth() + 10), jmax(1, getHeight()), true);
     image.clear(image.getBounds(), Colours::transparentBlack);
 
     // Draw a mid line on the image
     juce::Graphics imageGraphics(image);
     imageGraphics.setColour(juce::Colours::white);
     imageGraphics.drawLine(
-      0, getHeight() / 2, getWidth(), getHeight() / 2, 3.0f);
+      0, getHeight() / 2, getWidth() + 10, getHeight() / 2, 3.0f);
   }
   //==============================================================================
   void paint(juce::Graphics& g) noexcept override
@@ -55,64 +57,59 @@ public:
     const int samplesToRead = bufferSize - readPosition;
 
     const int maxSamplesToDraw = floor(samplesPerPixel * (float)width);
-    const int samplesToDraw = jmin(samplesToRead - 1, maxSamplesToDraw);
-    const int firstSamplesToDraw = readPosition; // bufferSize - samplesToDraw;
+    const int samplesToDraw = jmin(samplesToRead, maxSamplesToDraw);
+    const int firstSamplesToDraw = readPosition;
 
     const int pixelToDraw = samplesToDraw / samplesPerPixel;
     ringBuffer.incrementReadPosition(channel, samplesToDraw);
 
     // Image move
     const int destX = 0 - pixelToDraw;
-    image.moveImageSection(destX,   // destX
-                           0,       // destY
-                           0,       // srcX
-                           0,       // srcY
-                           width,   // width
-                           height); // height
+    image.moveImageSection(destX,      // destX
+                           0,          // destY
+                           0,          // srcX
+                           0,          // srcY
+                           width + 10, // width
+                           height);    // height
 
     // Clear the new part of the image
-    juce::Rectangle<int> clearRect(width - pixelToDraw, 0, pixelToDraw, height);
+    juce::Rectangle<int> clearRect(
+      width - pixelToDraw + 10, 0, pixelToDraw, height);
     image.clear(clearRect, juce::Colours::transparentBlack);
 
     // Generate path for new samples
-    const int drawStartX = width - pixelToDraw;
+    currentX = currentX - (int)currentX + width - pixelToDraw;
     float pixelsPerSample = 1.0f / samplesPerPixel;
 
-    const auto lastFullyDrawnSample =
-      juce::Point<float>(lastFullyDrawnSampleOffsetX + drawStartX,
-                         halfHeight + lastFullyDrawnSampleValue * halfHeight);
+    const float startY = halfHeight + currentSample * halfHeight;
+    const auto startPoint = Point(currentX, startY);
 
     juce::Path path;
-    path.startNewSubPath(lastFullyDrawnSample);
+    path.startNewSubPath(startPoint);
 
     for (int i = 0; i < samplesToDraw; ++i) {
       const int sampleIndex = firstSamplesToDraw + i;
-      const float sample = ringBuffer.getSample(channel, sampleIndex);
-      const float x = (float)drawStartX + i * pixelsPerSample;
-      const float y = halfHeight + sample * halfHeight;
-      const auto point = juce::Point<float>(x, y);
+      currentSample = ringBuffer.getSample(channel, sampleIndex);
+      currentX += pixelsPerSample;
+      const float y = halfHeight + currentSample * halfHeight;
+      const auto point = juce::Point<float>(currentX, y);
       path.lineTo(point);
-      if (i == samplesToDraw - 1) {
-        lastFullyDrawnSampleValue = sample;
-        lastFullyDrawnSampleOffsetX = x - width;
-        const auto lastSampleValue =
-          ringBuffer.getSample(channel, firstSamplesToDraw + samplesToDraw);
-        const auto lastSampleY = halfHeight + lastSampleValue * halfHeight;
-        const auto lastSampleX = x + pixelsPerSample;
-        path.lineTo(juce::Point<float>(lastSampleX, lastSampleY));
-      }
     }
+
+    PathStrokeType strokeType(3.0f,
+                              juce::PathStrokeType::JointStyle::curved,
+                              juce::PathStrokeType::EndCapStyle::butt);
 
     juce::Graphics imageGraphics(image);
     imageGraphics.setColour(juce::Colours::white);
-    imageGraphics.strokePath(path, juce::PathStrokeType(2.0f));
+    imageGraphics.strokePath(path, strokeType);
   }
 
 private:
   RingBuffer& ringBuffer;
   Image image;
-  SampleType lastFullyDrawnSampleValue;
-  float lastFullyDrawnSampleOffsetX;
+  SampleType currentSample;
+  float currentX;
   const int channel;
   //==============================================================================
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Oscilloscope)
