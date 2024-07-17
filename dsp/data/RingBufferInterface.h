08468a1a3484452a8654c2a369a0c2de9f08502c
David Hess <lunix@MacBook-Air-von-David.local>
2024-07-17 08:08:49 +0200

    Fixed MacOS build

--- dsp/data/RingBufferInterface.h
+++ dsp/data/RingBufferInterface.h
@@ -11,7 +11,6 @@ template<typename SampleType>
 class RingBufferInterface
 {
   using AudioBuffer = juce::AudioBuffer<SampleType>;
-  using Spans = std::vector<std::span<SampleType>>;
 
 public:
   //============================================================================
