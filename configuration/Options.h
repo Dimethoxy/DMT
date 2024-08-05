8adb54c760cffee7c753456ea63eb007b5873a82
Lunix-420 <davidhess336@gmx.de>
2024-08-05 23:42:27 +0200

    refactor: Update configuration module to handle platform-specific folder names

--- configuration/Options.h
+++ configuration/Options.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "utility/Settings.h"
 #include <JuceHeader.h>
 
 namespace dmt {
@@ -11,12 +12,20 @@ getOptions(juce::String applicationName)
   options.applicationName = applicationName;
   options.filenameSuffix = ".config";
   options.osxLibrarySubFolder = "Application Support";
-  options.folderName = "Dimethoxy";
   options.storageFormat = juce::PropertiesFile::storeAsXML;
   options.commonToAllUsers = false;
   options.ignoreCaseOfKeyNames = true;
   options.doNotSave = false;
   options.millisecondsBeforeSaving = -1; // Only when explicitly calling
+
+  if (OS_IS_WINDOWS) {
+    options.folderName = "Dimethoxy/" + applicationName;
+  } else if (OS_IS_DARWIN) {
+    options.folderName = "Dimethoxy/" + applicationName;
+  } else if (OS_IS_LINUX) {
+    options.folderName = ".config/Dimethoxy/" + applicationName;
+  }
+  
   return options;
 }
 } // namespace configuration
