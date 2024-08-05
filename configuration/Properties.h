131aed13c6b922eac3152fae099e7c962c8f3d39
Lunix-420 <davidhess336@gmx.de>
2024-08-05 17:58:31 +0200

    refactor: Update configuration module to use unique_ptr for properties

--- configuration/Properties.h
+++ configuration/Properties.h
@@ -1,16 +1,21 @@
 #pragma once
-
+//==============================================================================
 #include <JuceHeader.h>
 #include <configuration/Options.h>
-
+//==============================================================================
 namespace dmt {
 namespace configuration {
-static inline juce::PropertiesFile
+//==============================================================================
+std::unique_ptr<juce::PropertiesFile> properties;
+//==============================================================================
+void
 loadDefaultProperties(juce::String applicationName)
 {
   auto options = getOptions(applicationName);
-  auto properties = juce::PropertiesFile(options);
-  // return properties;
+  if (properties == nullptr) {
+    properties = std::make_unique<juce::PropertiesFile>(options);
+  }
 }
+//==============================================================================
 } // namespace configuration
 } // namespace dmt
