e5978b7df134c867aa5070e0999083c8d5c4154c
Lunix-420 <davidhess336@gmx.de>
2024-09-15 01:31:20 +0200

    Comment out problematic code

--- version/Manager.h
+++ version/Manager.h
@@ -1,48 +1,48 @@
-#pragma once
+// #pragma once
 
-#include <JuceHeader.h>
+// #include <JuceHeader.h>
 
-namespace dmt {
-namespace version {
-class Manager : public juce::Thread
-{
-public:
-  //============================================================================
-  Manager() = delete;
-  //============================================================================
-  Manager(const Manager& obj) = delete;
-  //============================================================================
-  ~Manager() { stopThread(1000); }
-  //============================================================================
-  static void initialize()
-  {
-    if (instance != nullptr) {
-      return;
-    }
-    instance = std::make_unique<Manager>();
-    instance->startThread();
-  }
+// namespace dmt {
+// namespace version {
+// class Manager : public juce::Thread
+// {
+// public:
+//   //============================================================================
+//   Manager() = delete;
+//   //============================================================================
+//   Manager(const Manager& obj) = delete;
+//   //============================================================================
+//   ~Manager() { stopThread(1000); }
+//   //============================================================================
+//   static void initialize()
+//   {
+//     if (instance != nullptr) {
+//       return;
+//     }
+//     instance = std::make_unique<Manager>();
+//     instance->startThread();
+//   }
 
-protected:
-  //============================================================================
-  void run() override
-  {
-    while (!threadShouldExit()) {
-      if (fetchLatestVersion()) {
-        break;
-      }
-      wait(1000);
-    }
-  }
-  //============================================================================
-  bool fetchLatestVersion()
-  {
-    // TODO: Implement this method and return true if successful
-    return false;
-  }
-  //============================================================================
-private:
-  static std::unique_ptr<Manager> instance;
-};
-} // namespace version
-} // namespace dmt
+// protected:
+//   //============================================================================
+//   void run() override
+//   {
+//     while (!threadShouldExit()) {
+//       if (fetchLatestVersion()) {
+//         break;
+//       }
+//       wait(1000);
+//     }
+//   }
+//   //============================================================================
+//   bool fetchLatestVersion()
+//   {
+//     // TODO: Implement this method and return true if successful
+//     return false;
+//   }
+//   //============================================================================
+// private:
+//   static std::unique_ptr<Manager> instance;
+// };
+// } // namespace version
+// } // namespace dmt
--- version/Networking.h
+++ version/Networking.h
@@ -1,31 +1,31 @@
-#pragma once
-//==============================================================================
-#include <JuceHeader.h>
-//==============================================================================
-namespace dmt {
-namespace version {
-//==============================================================================
-static const juce::String SERVER_URL = "https://api.dimethoxy.com/";
-//==============================================================================
-class Networking
-{
-public:
-  //==============================================================================
-  static juce::URL createURL(const juce::String& apiEndpoint)
-  {
-    return juce::URL("https://api.dimethoxy.com/" + apiEndpoint);
-  }
-  //==============================================================================
-  juce::String sendRequest(const juce::String& apiEndpoint)
-  {
-    juce::URL url = createURL(apiEndpoint);
+// #pragma once
+// //==============================================================================
+// #include <JuceHeader.h>
+// //==============================================================================
+// namespace dmt {
+// namespace version {
+// //==============================================================================
+// static const juce::String SERVER_URL = "https://api.dimethoxy.com/";
+// //==============================================================================
+// class Networking
+// {
+// public:
+//   //==============================================================================
+//   static juce::URL createURL(const juce::String& apiEndpoint)
+//   {
+//     return juce::URL("https://api.dimethoxy.com/" + apiEndpoint);
+//   }
+//   //==============================================================================
+//   juce::String sendRequest(const juce::String& apiEndpoint)
+//   {
+//     juce::URL url = createURL(apiEndpoint);
 
-    // Send a GET request to the specified URL
-    juce::String responseString = url.readEntireTextStream();
+//     // Send a GET request to the specified URL
+//     juce::String responseString = url.readEntireTextStream();
 
-    return responseString;
-  }
-  //==============================================================================
-};
-} // namespace version
-} // namespace dmt
+//     return responseString;
+//   }
+//   //==============================================================================
+// };
+// } // namespace version
+// } // namespace dmt
