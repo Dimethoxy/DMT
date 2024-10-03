a0a15768ca38150be169f6e88b72b4a5424fca1d
Lunix-420 <davidhess336@gmx.de>
2024-10-03 15:54:27 +0200

    refactor: DisfluxFilter

--- dsp/effect/DisfluxProcessor.h
+++ dsp/effect/DisfluxProcessor.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "dsp/filter/IIRFilter.h"
 #include <JuceHeader.h>
 
 namespace dmt {
@@ -7,42 +8,54 @@ namespace dsp {
 namespace effect {
 class DisfluxProcessor
 {
+  using AudioBuffer = juce::AudioBuffer<float>;
   using ProcessSpec = juce::dsp::ProcessSpec;
+  using Filter = dmt::dsp::filter::IIRFilterState;
+  using FilterArray = std::array<Filter, 2>;
+  using FilterMatrix = std::array<FilterArray, 2>;
 
 public:
-  DisfluxProcessor() {}
+  DisfluxProcessor() { setCoefficents(400.0f, 1.0f); }
 
   void prepare(double newSampleRate)
   {
-    sampleRate = newSampleRate;
-    auto maximumBlockSize = static_cast<juce::uint32>(4096);
-    auto processSpec = ProcessSpec{ sampleRate, maximumBlockSize, 2 };
-    for (auto& filter : filters) {
-      filter.prepare(processSpec);
+    this->sampleRate = (float)sampleRate;
+    for (auto& channel : filters) {
+      for (auto& filter : channel) {
+        filter.sampleRate = sampleRate;
+      }
     }
   }
-  void processBlock(juce::AudioBuffer<float>& buffer)
+  void processBlock(AudioBuffer& buffer)
   {
-    for (auto& filter : filters) {
-      processFilter(filter, buffer);
+    for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
+      auto* channelData = buffer.getWritePointer(channel);
+      auto channelFilters = filters[channel];
+      for (int i = 0; i < buffer.getNumSamples(); ++i) {
+        float sample = channelData[i];
+        for (auto& filter : channelFilters) {
+          sample = filter.base.processSingleSampleRaw(sample);
+        }
+        channelData[i] = sample;
+      }
     }
   }
 
 protected:
-  void processFilter(juce::dsp::IIR::Filter<float>& filter,
-                     juce::AudioBuffer<float>& buffer)
+  void setCoefficents(float frequency, float q)
   {
-    for (int channel = 0; channel < buffer.getNumChannels(); ++channel) {
-      auto* channelData = buffer.getWritePointer(channel);
-      for (int sample = 0; sample < buffer.getNumSamples(); ++sample) {
-        channelData[sample] = filter.processSample(channelData[sample]);
+    const auto coefficients =
+      juce::IIRCoefficients::makeHighPass(sampleRate, frequency, q);
+    for (auto& channel : filters) {
+      for (auto& filter : channel) {
+        filter.base.setCoefficients(coefficients);
       }
     }
   }
 
 private:
   float sampleRate = -1.0f;
-  std::array<juce::dsp::IIR::Filter<float>, 32> filters;
+  FilterMatrix filters;
 };
 } // namespace effect
 } // namespace dsp
--- dsp/filter/Filter.h
+++ dsp/filter/Filter.h
@@ -1,1 +1,3 @@
 #pragma once
+
+#include "IIRFilter.h"
