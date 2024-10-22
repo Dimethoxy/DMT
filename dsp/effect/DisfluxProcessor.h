acc5d1cace754bf49fb607063d661d944a975a8c
Lunix-420 <davidhess336@gmx.de>
2024-10-22 16:07:45 +0200

    disflux: Implement spread

--- dsp/effect/DisfluxProcessor.h
+++ dsp/effect/DisfluxProcessor.h
@@ -8,10 +8,11 @@ namespace dsp {
 namespace effect {
 class DisfluxProcessor
 {
+  constexpr static int FILTER_AMOUNT = 100;
   using AudioBuffer = juce::AudioBuffer<float>;
   using ProcessSpec = juce::dsp::ProcessSpec;
   using Filter = juce::IIRFilter;
-  using FilterArray = std::array<Filter, 100>;
+  using FilterArray = std::array<Filter, FILTER_AMOUNT>;
   const float& tolerance = dmt::Settings::floatTolerance;
 
 public:
@@ -31,13 +32,17 @@ public:
       return;
     }
 
+    const int newAmount = apvts.getRawParameterValue("DisfluxAmount")->load();
+    const int newSpread = apvts.getRawParameterValue("DisfluxSpread")->load();
     const auto newFrequency =
       apvts.getRawParameterValue("DisfluxFrequency")->load();
     const auto newPinch = apvts.getRawParameterValue("DisfluxPinch")->load();
-    const auto amount = apvts.getRawParameterValue("DisfluxAmount")->load();
 
-    if (std::abs(newFrequency - frequency) > tolerance ||
-        std::abs(newPinch - pinch) > tolerance) {
+    if (amount != newAmount || spread != newSpread ||
+        std::abs(frequency - newFrequency) > tolerance ||
+        std::abs(pinch - newPinch) > tolerance) {
+      amount = newAmount;
+      spread = newSpread;
       frequency = newFrequency;
       pinch = newPinch;
       setCoefficents();
@@ -60,20 +65,47 @@ public:
 protected:
   void setCoefficents()
   {
-    const auto coefficients =
-      juce::IIRCoefficients::makeAllPass((double)sampleRate, frequency, pinch);
+    // const auto coefficients =
+    //   juce::IIRCoefficients::makeAllPass((double)sampleRate, frequency,
+    //   pinch);
 
-    for (auto& filter : leftFilters) {
-      filter.setCoefficients(coefficients);
+    const float spreadAmount = (float)spread;
+    const float maxFrequency = 20000.0f;
+    const float rangeStartFrequency =
+      std::clamp(frequency - (spreadAmount / 2.0f), 20.0f, 20000.0f);
+    const float rangeEndFrequency =
+      std::clamp(frequency + (spreadAmount / 2.0f), 20.0f, 20000.0f);
+
+    const float logStartFrequency = std::log(rangeStartFrequency);
+    const float logEndFrequency = std::log(rangeEndFrequency);
+    const float logFrequencyDelta = logEndFrequency - logStartFrequency;
+
+    for (size_t filterIndex = 0; filterIndex < amount; ++filterIndex) {
+      const float logFrequencyOffsetFactor =
+        (amount == 1) ? 0.5f : (float)filterIndex / (amount - 1);
+
+      const float logFrequency = std::exp(
+        logStartFrequency + (logFrequencyDelta * logFrequencyOffsetFactor));
+
+      const auto coefficients = juce::IIRCoefficients::makeAllPass(
+        (double)sampleRate, logFrequency, pinch);
+
+      leftFilters[filterIndex].setCoefficients(coefficients);
+      rightFilters[filterIndex].setCoefficients(coefficients);
     }
-    for (auto& filter : rightFilters) {
-      filter.setCoefficients(coefficients);
-    }
+    // for (auto& filter : leftFilters) {
+    //   filter.setCoefficients(coefficients);
+    // }
+    // for (auto& filter : rightFilters) {
+    //   filter.setCoefficients(coefficients);
+    // }
   }
 
 private:
   juce::AudioProcessorValueTreeState& apvts;
   float sampleRate = -1.0f;
+  int amount = 1;
+  int spread = 0;
   float frequency = 800.0f;
   float pinch = 1.0f;
   FilterArray leftFilters;
