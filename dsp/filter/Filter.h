9a68234d1d9a7644949cd8a71ed7a94c3f79fb9d
Lunix-420 <davidhess336@gmx.de>
2025-02-11 00:06:34 +0100

    refactor: Simplify Filter.h by removing LowPassFilter class and updating header description

--- dsp/filter/Filter.h
+++ dsp/filter/Filter.h
@@ -19,7 +19,7 @@
  * Your are not allowed to use this code in any closed-source project.
  *
  * Description:
- * Get the options for the properties file with predefined settings.
+ * Filter header file.
  *
  * Authors:
  * Lunix-420 (Primary Author)
@@ -27,61 +27,5 @@
 //==============================================================================
 
 #pragma once
-
-//==============================================================================
-
-#include <algorithm>
-#include <cmath>
-
-//==============================================================================
-
-namespace dmt {
-namespace dsp {
-namespace filter {
-
-//==============================================================================
-
-/**
- * @brief Simple low-pass filter
- *
- * This class implements a simple low-pass filter using a one-pole design.
- */
-class LowPassFilter
-{
-public:
-  /**
-   * @brief Set the cutoff frequency of the filter.
-   * @param _cutoffFrequency The cutoff frequency in Hz.
-   * @param _sampleRate The sample rate in Hz.
-   */
-  inline void setCutoffFrequency(const float _cutoffFrequency,
-                                 const float _sampleRate) noexcept
-  {
-    const float RC =
-      1.0f / (2.0f * static_cast<float>(M_PI) * _cutoffFrequency);
-    alpha = RC / (RC + (1.0f / _sampleRate));
-  }
-
-  /**
-   * @brief Process a single sample through the filter.
-   * @param _input The input sample.
-   * @return The filtered sample.
-   */
-  [[nodiscard]] inline float processSample(const float _input) noexcept
-  {
-    previousOutput = alpha * _input + (1.0f - alpha) * previousOutput;
-    return previousOutput;
-  }
-
-private:
-  float alpha = 0.0f;
-  float previousOutput = 0.0f;
-};
-
-//==============================================================================
-
-} // namespace filter
-} // namespace dsp
-} // namespace dmt
 
 //==============================================================================
