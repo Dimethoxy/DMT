c59367fbda103dd2b6502e15ae7635b52c5e3798
Lunix-420 <davidhess336@gmx.de>
2025-05-08 11:07:54 +0200

    feat(model): Add more params

--- model/HereticParameters.h
+++ model/HereticParameters.h
@@ -25,13 +25,20 @@ hereticParameterGroup(juce::String parentUid)
     uid,       // group ID
     "Heretic", // group name
     "|",       // separator
-    std::make_unique<ParameterFloat>(uid + "Drive",
-                                     "Drive",
-                                     NormalisableRange(1.0f,  // rangeStart
-                                                       10.0f, // rangeEnd
+    std::make_unique<ParameterFloat>(uid + "PreGain",
+                                     "PreGain",
+                                     NormalisableRange(0.0f,  // rangeStart
+                                                       1.0f,  // rangeEnd
                                                        0.01f, // intervalValue
-                                                       0.5f), // skewFactor
+                                                       1.0f), // skewFactor
                                      1.0f),                   // defaultValue
+    std::make_unique<ParameterFloat>(uid + "Spread",
+                                     "Spread",
+                                     NormalisableRange(0.0f,  // rangeStart
+                                                       1.0f,  // rangeEnd
+                                                       0.01f, // intervalValue
+                                                       1.0f), // skewFactor
+                                     0.5f),                   // defaultValue
     std::make_unique<ParameterFloat>(uid + "Range",
                                      "Range",
                                      NormalisableRange(0.0f,  // rangeStart
@@ -39,13 +46,43 @@ hereticParameterGroup(juce::String parentUid)
                                                        0.01f, // intervalValue
                                                        1.0f), // skewFactor
                                      8.0f),                   // defaultValue
-    std::make_unique<ParameterFloat>(uid + "Tone",
-                                     "Tone",
-                                     NormalisableRange(20.0f,    // rangeStart
-                                                       20000.0f, // rangeEnd
-                                                       1.0f,   // intervalValue
-                                                       0.25f), // skewFactor
-                                     20000.0f),                // defaultValue
+    // defaultValue
+    std::make_unique<ParameterFloat>(uid + "Mix",
+                                     "Mix",
+                                     NormalisableRange(0.0f,  // rangeStart
+                                                       1.0f,  // rangeEnd
+                                                       0.01f, // intervalValue
+                                                       1.0f), // skewFactor
+                                     0.5f),                   // defaultValue
+    std::make_unique<ParameterFloat>(uid + "Distortion",
+                                     "Distortion",
+                                     NormalisableRange(0.0f,  // rangeStart
+                                                       1.0f,  // rangeEnd
+                                                       0.01f, // intervalValue
+                                                       1.0f), // skewFactor
+                                     0.1f),                   // defaultValue
+    std::make_unique<ParameterFloat>(uid + "Drive",
+                                     "Drive",
+                                     NormalisableRange(0.0f,  // rangeStart
+                                                       1.0f,  // rangeEnd
+                                                       0.01f, // intervalValue
+                                                       1.0f), // skewFactor
+                                     0.5f),                   // defaultValue
+    std::make_unique<AudioParameterChoice>(uid + "DriveType",
+                                           "Drive Type",
+                                           juce::StringArray{ "Hard Clipping",
+                                                              "Soft Clipping",
+                                                              "Saturate",
+                                                              "Atan",
+                                                              "Crunch",
+                                                              "Bitcrush",
+                                                              "Extreme",
+                                                              "Screame",
+                                                              "Sine",
+                                                              "Cosine",
+                                                              "Harmonic",
+                                                              "Weird" },
+                                           0), // defaultItemIndex
     std::make_unique<ParameterFloat>(uid + "Feedback",
                                      "Feedback",
                                      NormalisableRange(0.0f,  // rangeStart
@@ -53,14 +90,28 @@ hereticParameterGroup(juce::String parentUid)
                                                        0.01f, // intervalValue
                                                        1.0f), // skewFactor
                                      0.0f),                   // defaultValue
-    std::make_unique<ParameterFloat>(uid + "Mix",
-                                     "Mix",
-                                     NormalisableRange(0.0f,  // rangeStart
-                                                       1.0f,  // rangeEnd
-                                                       0.01f, // intervalValue
-                                                       1.0f), // skewFactor
-                                     0.5f)                    // defaultValue
-  );
+    std::make_unique<AudioParameterChoice>(uid + "FeedbackFilterSlope",
+                                           "Feedback Filter Slope",
+                                           juce::StringArray{ "LP 12dB/Oct",
+                                                              "LP 24dB/Oct",
+                                                              "LP 36dB/Oct",
+                                                              "LP 48dB/Oct",
+                                                              "BP 12dB/Oct",
+                                                              "BP 24dB/Oct",
+                                                              "BP 36dB/Oct",
+                                                              "BP 48dB/Oct",
+                                                              "HP 12dB/Oct",
+                                                              "HP 24dB/Oct",
+                                                              "HP 36dB/Oct",
+                                                              "HP 48dB/Oct" },
+                                           0), // defaultItemIndex
+    std::make_unique<ParameterFloat>(uid + "FeedbackFilterCutoff",
+                                     "Feedback Filter Cutoff",
+                                     NormalisableRange(20.0f,    // rangeStart
+                                                       20000.0f, // rangeEnd
+                                                       1.0f,   // intervalValue
+                                                       0.25f), // skewFactor
+                                     20000.0f));
 }
 } // namespace model
 } // namespace dmt
