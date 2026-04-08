82761054cff0e87f8dd94049a1cecf3fbfad8efc
Lunix-420 <davidhess336@gmx.de>
2026-04-08 11:49:25 +0200

    app: refactor plugin editor to inherit from AbstractPluginEditor

--- app/AbstractPluginEditor.h
+++ app/AbstractPluginEditor.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "./AbstractPluginProcessor.h"
 #include <JuceHeader.h>
+
+namespace dmt {
+namespace app {
+class AbstractPluginEditor : public juce::AudioProcessorEditor
+{
+public:
+  explicit AbstractPluginEditor(dmt::app::AbstractPluginProcessor& p)
+    : juce::AudioProcessorEditor(&p)
+  {
+  }
+
+  ~AbstractPluginEditor() override = default;
+};
+} // namespace app
+} // namespace dmt
